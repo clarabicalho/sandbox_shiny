d063fecce8ae0930bbe2fe361e0f874a460ee6f7
Jasper Cooper <jaspercooper@gmail.com>
2018-05-08 20:26:16 -0400

    Script for preparing RDS files for shiny app up
    - for now we're just putting these in the data folder, but it could be modified to put them elsewhere, outside the package root

---
+++ other/prepare_shiny_data.R
@@ -0,0 +1,29 @@
+
+functions <- ls("package:DesignLibrary")
+designers <- functions[grepl("_designer\\b",functions)]
+sims <- 2
+bootstraps <- 2
+
+for(designer in designers){
+
+  the_designer <- get(x = designer)
+  has_shiny <- !is.null(attributes(the_designer)$shiny_arguments)
+  designer_args <- formals(the_designer)
+  designer_attr <- attributes(the_designer)
+  one_design <- the_designer()
+  design_attr <- attributes(one_design)
+
+
+  if(has_shiny){
+    get_or_run_shiny_diagnosis(
+      designer = the_designer,
+      designer_name = designer,
+      sims = sims,
+      bootstrap = bootstraps,
+      update_existing = TRUE
+    )
+  }
+}
+
+
+

9eb46505f9682d0101e1f4fc4e09aeb567ffef9c
Clara Bicalho <cbm343@nyu.edu>
2018-11-22 16:49:13 +0100

    Add functions to sandbox

--- R/sandbox_fns.R
+++ R/sandbox_fns.R
@@ -32,6 +32,83 @@ attr(population_3, "label") <- deparse(substitute(population_3))
 attr(population_3, "description") <- "N obs w/ blocks, Y and Z cov"
 attr(population_3, "step_type") <- "population"
 
+
+# sampling steps ----------------------------------------------------------
+
+# With n
+sampling_1 <- function(n = NULL) eval_bare(expr(declare_sampling(n = !!n)))
+attr(sampling_1, "label") <- deparse(substitute(sampling_1))
+attr(sampling_1, "description") <- "Sample n"
+attr(sampling_1, "step_type") <- "sampling"
+
+sampling_2 <- function(n = NULL, strata){
+  eval_bare(expr(declare_sampling(n = !!n, strata = strata)))
+}
+attr(sampling_2, "label") <- deparse(substitute(sampling_2))
+attr(sampling_2, "description") <- "Sample n with strata"
+attr(sampling_2, "step_type") <- "sampling"
+attr(sampling_2, "expects") <- "strata"
+
+sampling_3 <- function(n = NULL, strata, clusters){
+  eval_bare(expr(declare_sampling(n = !!n,
+                                  strata = !!sym(strata),
+                                  clusters = !!sym(clusters))))
+}
+attr(sampling_3, "label") <- deparse(substitute(sampling_3))
+attr(sampling_3, "description") <- "Sample n with strata and clusters"
+attr(sampling_3, "step_type") <- "sampling"
+attr(sampling_3, "expects") <- c("strata", "clusters")
+
+
+sampling_4 <- function(prob = NULL){
+  eval_bare(expr(declare_sampling(prob = !!prob,
+                                  simple = FALSE)))
+}
+attr(sampling_4, "label") <- deparse(substitute(sampling_4))
+attr(sampling_4, "description") <- "Sample with probability"
+attr(sampling_4, "step_type") <- "sampling"
+
+# # With prob +
+# # With strata only
+# sampling_5 <- function(prob = NULL, strata) eval_bare(expr(
+#   declare_sampling(prob = !!prob, strata = !!sym(strata))
+# ))
+#
+# # With strata and clusters
+# sampling_6 <- function(prob = NULL, strata, clusters) eval_bare(expr({
+#   declare_sampling(prob = !!prob,
+#                    strata = !!sym(strata),
+#                    clusters = !!sym(clusters))
+# }))
+#
+# # With strata_prob +
+# # With strata only
+# sampling_7 <- function(strata_prob = NULL, strata) eval_bare(expr({
+#   declare_sampling(strata_prob = !!strata_prob,
+#                    strata = !!sym(strata))
+# }))
+#
+# # With strata and clusters
+# sampling_8 <- function(strata_prob = NULL, strata, clusters) eval_bare(expr({
+#   declare_sampling(strata_prob = !!strata_prob,
+#                    strata = !!sym(strata),
+#                    clusters = !!sym(clusters))
+# }))
+#
+# # With strata_n +
+# # With strata only
+# sampling_9 <- function(strata_n = NULL, strata) eval_bare(expr({
+#   declare_sampling(strata_n = !!strata_n,
+#                    strata = !!sym(strata))
+# }))
+#
+# # With strata and clusters
+# sampling_10 <- function(strata_n = NULL, strata, clusters) eval_bare(expr({
+#   declare_sampling(strata_n = !!strata_n,
+#                    strata = !!sym(strata),
+#                    clusters = !!sym(clusters))
+# }))
+
 # assignment steps --------------------------------------------------------
 
 # Declare assignment of m units to treatment
@@ -43,7 +120,7 @@ attr(assignment_1, "step_type") <- "assignment"
 # Declare assignment specifying assignment probability for each block
 assignment_2 <- function(block_prob = "1/3, 2/3", blocks_var = "block"){
   eval_bare(expr({
-    declare_assignment(block_prob = !!parse(text = paste0("c(","1/3, 2/3",")")),
+    declare_assignment(block_prob = !!parse(text = paste0("c(",block_prob,")")),
                        blocks = !!sym(blocks_var))
   }))
 }
@@ -51,15 +128,14 @@ attr(assignment_2, "label") <- deparse(substitute(assignment_2))
 attr(assignment_2, "description") <- "Block"
 attr(assignment_2, "step_type") <- "assignment"
 
-
-# Declare assignment of clusters with probability 1/4
+# Declare assignment of clusters with probability
 assignment_3 <- function(probability = .5, cluster_var = "cluster", assignment_var = "Z"){
   eval_bare(expr({
     declare_assignment(prob = !!probability,
                        clusters = !!sym(cluster_var),
                        assignment_variable = !!assignment_var)
   }))
-  }
+}
 attr(assignment_3, "label") <- deparse(substitute(assignment_3))
 attr(assignment_3, "description") <- "Cluster"
 attr(assignment_3, "step_type") <- "assignment"
@@ -70,15 +146,13 @@ attr(assignment_4, "label") <- deparse(substitute(assignment_4))
 attr(assignment_4, "description") <- "Multi-arm"
 attr(assignment_4, "step_type") <- "assignment"
 
-# Declare assignment using custom handler
-#
-# custom_assignment <- function(data, assignment_variable = "X") {
-#   data[, assignment_variable] <- rbinom(n = nrow(data),
-#                                         size = 1,
-#                                         prob = 0.5)
-#   data
-# }
-
+# Assign conditions in a two-by-two design
+# Factorial can be done with two assignment_3 steps
+# assignment_5 <- function(prob_A, prob_B, factor_name_1, factor_name_2) eval_bare(expr({
+#   assign_A <- declare_assignment(prob = !!prob_A, assignment_variable = !!factor_name_1)
+#   assign_B <- declare_assignment(prob = !!prob_B, assignment_variable = !!factor_name_2,
+#                      blocks = !!sym(factor_name_1))
+#   }))
 
 # other steps -------------------------------------------------------------
 
@@ -89,7 +163,7 @@ add_dv <- function(lambda = .9){
       Y[t == 1] <- 1 + u_i[t == 1]
       for(j in 2:max(t))  {Y[t==j] <- (1-!!lambda) + !!lambda*Y[t==(j - 1)] + u_i[t==j]}
       })})})
-    )
+  )
 }
 
 attr(add_dv, "label") <- deparse(substitute(add_dv))
@@ -139,3 +213,4 @@ sandbox <- do.call(list, mget(setdiff(names(.GlobalEnv), ".Random.seed")))
 #     updateShinyInput(session, id, updates[[id]])
 #   })
 # }
+
